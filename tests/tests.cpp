512187f242c3b5dfb02d40158a25ce788dec0cbd
Boris Kozlov <kozlovb82@yahoo.fr>
2024-10-15 22:48:52 +0200

    Added cmake

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,43 +1,44 @@
-#include "microtest.h"
+#include <gtest/gtest.h>  // Google Test
 #include "grep_utils.h"
 #include "grep_interface_functions.h"
+#include <sstream>
+#include <filesystem>
 
 namespace grep_tests
 {
-  TEST(ReadGrepArgumentsTest_ValidInput)
+  TEST(ReadGrepArgumentsTest, ValidInput)
   {
     const char *argv[] = {"program", "arg1 arg1", "arg2"};
     int argc = 3;
 
     auto result = grep::read_grep_arguments(argc, const_cast<char **>(argv));
 
-    ASSERT_EQ(result.first, "arg1 arg1");
-    ASSERT_EQ(result.second, "arg2");
+    EXPECT_EQ(result.first, "arg1 arg1");
+    EXPECT_EQ(result.second, "arg2");
   }
 
-  TEST(ReadGrepArgumentsTest_LessThanTwoArguments)
+  TEST(ReadGrepArgumentsTest, LessThanTwoArguments)
   {
     const char *argv[] = {"program", "arg1"};
     int argc = 2;
 
     auto result = grep::read_grep_arguments(argc, const_cast<char **>(argv));
 
-    ASSERT_EQ(result.first, "");
-    ASSERT_EQ(result.second, "");
+    EXPECT_EQ(result.first, "");
+    EXPECT_EQ(result.second, "");
   }
 
-  TEST(ReadGrepArgumentsTest_MoreThanTwoArguments)
+  TEST(ReadGrepArgumentsTest, MoreThanTwoArguments)
   {
     const char *argv[] = {"program", "arg1", "arg2", "arg3"};
     int argc = 4;
 
     auto result = grep::read_grep_arguments(argc, const_cast<char **>(argv));
 
-    ASSERT_EQ(result.first, "");
-    ASSERT_EQ(result.second, "");
+    EXPECT_EQ(result.first, "");
+    EXPECT_EQ(result.second, "");
   }
 
-  // todo move
   namespace fs = std::filesystem;
 
   auto file_collector = [](std::vector<fs::path> &collected_files)
@@ -48,34 +49,34 @@ namespace grep_tests
     };
   };
 
-  TEST(FindFilesTest_PathDoesNotExist)
+  TEST(FindFilesTest, PathDoesNotExist)
   {
     std::vector<fs::path> files;
-    std::string non_existent_path = "./test/non_existent_directory";
+    std::string non_existent_path = "./test_assets/non_existent_directory";
     grep::find_files(non_existent_path, file_collector(files));
-    ASSERT_TRUE(files.empty());
+    EXPECT_TRUE(files.empty());
   }
 
-  TEST(FindFilesTest_SingleRegularFile)
+  TEST(FindFilesTest, SingleRegularFile)
   {
     std::vector<fs::path> files;
-    fs::path single_file = "./test/single_file.txt";
+    fs::path single_file = "./test_assets/single_file.txt";
     grep::find_files(single_file, file_collector(files));
-    ASSERT_EQ(files.size(), 1);
-    ASSERT_EQ(files.front(), single_file);
+    EXPECT_EQ(files.size(), 1);
+    EXPECT_EQ(files.front(), single_file);
   }
 
-  TEST(FindFilesTest_DirectoryWithFiles)
+  TEST(FindFilesTest, DirectoryWithFiles)
   {
     std::vector<fs::path> files;
-    fs::path directory = "./test/test_directory";
+    fs::path directory = "./test_assets/test_directory";
     grep::find_files(directory, file_collector(files));
-    ASSERT_EQ(files.size(), 2);
-    ASSERT_EQ(files[0], "./test/test_directory/test.txt");
-    ASSERT_EQ(files[1], "./test/test_directory/subdir/test.txt");
+    EXPECT_EQ(files.size(), 2);
+    EXPECT_EQ(files[0], "./test_assets/test_directory/test.txt");
+    EXPECT_EQ(files[1], "./test_assets/test_directory/subdir/test.txt");
   }
 
-  TEST(OutputColoredResultTest_WithMatched)
+  TEST(OutputColoredResultTest, WithMatched)
   {
     grep::Result::LineMatchResults line_match_results = {
         {"This is a test line with a match", {grep::MatchPosition(27, 31)}},
@@ -95,19 +96,19 @@ namespace grep_tests
                                                                                                      "test_file.txt: Another " +
         grep::ColorStart + "line" + grep::ColorEnd + " with " + grep::ColorStart + "multiple" + grep::ColorEnd + " matches\n";
 
-    ASSERT_EQ(outputBuffer.str(), expectedOutput);
+    EXPECT_EQ(outputBuffer.str(), expectedOutput);
   }
 
-  TEST(SearchInFileTest)
+  TEST(SearchInFileTest, SearchMatchInFile)
   {
     grep::Result::LineMatchResults line_match_results = {
         {"matchmatchgmatch", {grep::MatchPosition(0, 4), grep::MatchPosition(5, 9), grep::MatchPosition(11, 15)}},
         {"match", {grep::MatchPosition(0, 4)}}};
 
-    grep::Result expectedResult("./test/single_file.txt", std::move(line_match_results));
-    const auto actualResult = grep::search_in_file("match", "./test/single_file.txt");
+    grep::Result expectedResult("./test_assets/single_file.txt", std::move(line_match_results));
+    const auto actualResult = grep::search_in_file("match", "./test_assets/single_file.txt");
 
-    ASSERT_TRUE(expectedResult == actualResult);
+    EXPECT_TRUE(expectedResult == actualResult);
   }
 
   // Helper function to split string by lines
@@ -122,47 +123,46 @@ namespace grep_tests
     }
     return lines;
   }
-  TEST(GrepTest_WithMatched)
+
+  TEST(GrepTest, WithMatched)
   {
     std::stringstream outputBuffer;
     const auto oldCoutStreamBuf = std::cout.rdbuf();
     std::cout.rdbuf(outputBuffer.rdbuf());
 
-    grep::grep("match", "./test/");
+    grep::grep("match", "./test_assets/");
 
     std::cout.rdbuf(oldCoutStreamBuf);
 
     std::vector<std::string> expectedLines = {
-        "./test/test_directory/subdir/test.txt: iii" + grep::ColorStart + "match" + grep::ColorEnd + "iii" + "\n",
-        "./test/single_file.txt: " + grep::ColorStart + "match" + grep::ColorEnd + grep::ColorStart + "match" + grep::ColorEnd + "g" + grep::ColorStart + "match" + grep::ColorEnd + "\n",
-        "./test/single_file.txt: " + grep::ColorStart + "match" + grep::ColorEnd + "\n"};
+        "./test_assets/test_directory/subdir/test.txt: iii" + grep::ColorStart + "match" + grep::ColorEnd + "iii" + "\n",
+        "./test_assets/single_file.txt: " + grep::ColorStart + "match" + grep::ColorEnd + grep::ColorStart + "match" + grep::ColorEnd + "g" + grep::ColorStart + "match" + grep::ColorEnd + "\n",
+        "./test_assets/single_file.txt: " + grep::ColorStart + "match" + grep::ColorEnd + "\n"};
 
-    // Split the actual output into lines
     std::vector<std::string> actualLines = splitLines(outputBuffer.str());
 
-    // Sort both the expected and actual output, as order doesn't matter
     std::sort(expectedLines.begin(), expectedLines.end());
     std::sort(actualLines.begin(), actualLines.end());
 
-    ASSERT_EQ(actualLines.size(), expectedLines.size());
+    EXPECT_EQ(actualLines.size(), expectedLines.size());
     for (size_t i = 0; i < expectedLines.size(); ++i)
     {
-      ASSERT_EQ(actualLines[i], expectedLines[i]);
+      EXPECT_EQ(actualLines[i], expectedLines[i]);
     }
   }
 
-  TEST(GrepTest_NoMatches)
+  TEST(GrepTest, NoMatches)
   {
     std::stringstream outputBuffer;
     const auto oldCoutStreamBuf = std::cout.rdbuf();
     std::cout.rdbuf(outputBuffer.rdbuf());
-    grep::grep("notfound", "./test/");
+    grep::grep("notfound", "./test_assets/");
     std::cout.rdbuf(oldCoutStreamBuf);
 
-    ASSERT_TRUE(outputBuffer.str().empty());
+    EXPECT_TRUE(outputBuffer.str().empty());
   }
 
-  TEST(GrepTest_WrongPath)
+  TEST(GrepTest, WrongPath)
   {
     std::stringstream outputBuffer;
     const auto oldCoutStreamBuf = std::cout.rdbuf();
@@ -170,8 +170,12 @@ namespace grep_tests
     grep::grep("notfound", "./test_wrong/");
     std::cout.rdbuf(oldCoutStreamBuf);
 
-    ASSERT_TRUE(outputBuffer.str().empty());
+    EXPECT_TRUE(outputBuffer.str().empty());
   }
 }
 
-TEST_MAIN();
+int main(int argc, char **argv)
+{
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
